2d7fecb5731b70afc7f688bb1b8d7e17008b843e
Ashraf Abdelrahman <96283360+AshrafAKRahman@users.noreply.github.com>
2023-01-29 18:12:31 +0000

    created a loading message while data is being fetched

--- src/components/NewsGrid.js
+++ src/components/NewsGrid.js
@@ -4,9 +4,11 @@ import NewsItem from "./NewsItem";
 export default function NewsGrid({ items }) {
   return (
     <div className="news-grid">
-      {items.map((item, i) => (
-        <NewsItem key={i} item={item} />
-      ))}
+      {items ? (
+        items.map((item, i) => <NewsItem key={i} item={item} />)
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
